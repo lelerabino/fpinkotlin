d9cc7d4eec02b344be943eae9a0480f108df3373
Emanuele Rabino <e.rabino@teamsystem.com>
2022-01-02 14:20:09 +0100

    test: fix leftover test activation

--- src/test/kotlin/chapter3/exercises/ex16/listing.kt
+++ src/test/kotlin/chapter3/exercises/ex16/listing.kt
@@ -15,7 +15,7 @@ fun doubleToString(xs: List<Double>): List<String> =
 //TODO: Enable tests by removing `!` prefix
 class Exercise16 : WordSpec({
     "list doubleToString" should {
-        "!convert every double element to a string" {
+        "convert every double element to a string" {
             doubleToString(List.of(1.1, 1.2, 1.3, 1.4)) shouldBe
                 List.of("1.1", "1.2", "1.3", "1.4")
         }

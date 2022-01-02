57648f756dbdf4e500d6a3eab2f6ec509e5a9b32
Emanuele Rabino <e.rabino@teamsystem.com>
2022-01-02 14:31:41 +0100

    feat: List generic flatMap exercise

--- src/test/kotlin/chapter3/exercises/ex19/listing.kt
+++ src/test/kotlin/chapter3/exercises/ex19/listing.kt
@@ -1,20 +1,21 @@
 package chapter3.exercises.ex19
 
 import chapter3.List
+import chapter3.append
+import chapter3.foldRight
 import io.kotlintest.shouldBe
 import io.kotlintest.specs.WordSpec
-import utils.SOLUTION_HERE
 
 // tag::init[]
 fun <A, B> flatMap(xa: List<A>, f: (A) -> List<B>): List<B> =
+    foldRight(xa, List.empty()) { a, xb -> append(f(a), xb) }
 
-    SOLUTION_HERE()
 // end::init[]
 
 //TODO: Enable tests by removing `!` prefix
 class Exercise19 : WordSpec({
     "list flatmap" should {
-        "!map and flatten a list" {
+        "map and flatten a list" {
             val xs = List.of(1, 2, 3)
             flatMap(xs) { i -> List.of(i, i) } shouldBe
                 List.of(1, 1, 2, 2, 3, 3)

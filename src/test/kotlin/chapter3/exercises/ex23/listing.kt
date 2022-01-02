cf35a382498b471b7063579aadb3afe4f04892a7
Emanuele Rabino <e.rabino@teamsystem.com>
2022-01-02 15:22:11 +0100

    feat: List startsWith and hasSubsequence exercise

--- src/test/kotlin/chapter3/exercises/ex23/listing.kt
+++ src/test/kotlin/chapter3/exercises/ex23/listing.kt
@@ -1,47 +1,66 @@
 package chapter3.exercises.ex23
 
+import chapter3.Cons
 import chapter3.List
+import chapter3.Nil
 import io.kotlintest.shouldBe
 import io.kotlintest.specs.WordSpec
-import utils.SOLUTION_HERE
 
 // tag::startsWith[]
 tailrec fun <A> startsWith(l1: List<A>, l2: List<A>): Boolean =
-
-    SOLUTION_HERE()
-// end::startsWith[]
-
-// tag::init[]
-tailrec fun <A> hasSubsequence(xs: List<A>, sub: List<A>): Boolean =
-
-    SOLUTION_HERE()
-// end::init[]
-
-//TODO: Enable tests by removing `!` prefix
-class Exercise23 : WordSpec({
-    "list subsequence" should {
-        "!determine if a list starts with" {
-            val xs = List.of(1, 2, 3)
-            startsWith(xs, List.of(1)) shouldBe true
-            startsWith(xs, List.of(1, 2)) shouldBe true
-            startsWith(xs, xs) shouldBe true
-            startsWith(xs, List.of(2, 3)) shouldBe false
-            startsWith(xs, List.of(3)) shouldBe false
-            startsWith(xs, List.of(6)) shouldBe false
-        }
-
-        "!identify subsequences of a list" {
-            val xs = List.of(1, 2, 3, 4, 5)
-            hasSubsequence(xs, List.of(1)) shouldBe true
-            hasSubsequence(xs, List.of(1, 2)) shouldBe true
-            hasSubsequence(xs, List.of(2, 3)) shouldBe true
-            hasSubsequence(xs, List.of(3, 4)) shouldBe true
-            hasSubsequence(xs, List.of(3, 4, 5)) shouldBe true
-            hasSubsequence(xs, List.of(4)) shouldBe true
-
-            hasSubsequence(xs, List.of(1, 4)) shouldBe false
-            hasSubsequence(xs, List.of(1, 3)) shouldBe false
-            hasSubsequence(xs, List.of(2, 4)) shouldBe false
+    when (l1) {
+        is Nil -> l2 == Nil
+        is Cons -> when (l2) {
+            is Nil -> true
+            is Cons -> when {
+                (l1.head != l2.head) -> false
+                else -> startsWith(
+                    l1.tail,
+                    l2.tail
+                )
+            }
         }
     }
-})
+
+// end::startsWith[]
+
+    // tag::init[]
+    tailrec fun <A> hasSubsequence(xs: List<A>, sub: List<A>): Boolean =
+        when(xs){
+            is Nil -> sub == Nil
+            is Cons -> when{
+                startsWith(xs, sub) -> true
+                else -> hasSubsequence(xs.tail, sub)
+            }
+        }
+
+
+// end::init[]
+
+    class Exercise23 : WordSpec({
+        "list subsequence" should {
+            "determine if a list starts with" {
+                val xs = List.of(1, 2, 3)
+                startsWith(xs, List.of(1)) shouldBe true
+                startsWith(xs, List.of(1, 2)) shouldBe true
+                startsWith(xs, xs) shouldBe true
+                startsWith(xs, List.of(2, 3)) shouldBe false
+                startsWith(xs, List.of(3)) shouldBe false
+                startsWith(xs, List.of(6)) shouldBe false
+            }
+
+            "identify subsequences of a list" {
+                val xs = List.of(1, 2, 3, 4, 5)
+                hasSubsequence(xs, List.of(1)) shouldBe true
+                hasSubsequence(xs, List.of(1, 2)) shouldBe true
+                hasSubsequence(xs, List.of(2, 3)) shouldBe true
+                hasSubsequence(xs, List.of(3, 4)) shouldBe true
+                hasSubsequence(xs, List.of(3, 4, 5)) shouldBe true
+                hasSubsequence(xs, List.of(4)) shouldBe true
+
+                hasSubsequence(xs, List.of(1, 4)) shouldBe false
+                hasSubsequence(xs, List.of(1, 3)) shouldBe false
+                hasSubsequence(xs, List.of(2, 4)) shouldBe false
+            }
+        }
+    })

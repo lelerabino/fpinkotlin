99b278e53ff9317e5756cb1b7f4c4b14d053ad8b
Emanuele Rabino <e.rabino@teamsystem.com>
2022-01-02 14:14:10 +0100

    feat: List of Int increment exercise

--- src/test/kotlin/chapter3/exercises/ex15/listing.kt
+++ src/test/kotlin/chapter3/exercises/ex15/listing.kt
@@ -1,20 +1,21 @@
 package chapter3.exercises.ex15
 
+import chapter3.Cons
 import chapter3.List
+import chapter3.foldRight
 import io.kotlintest.shouldBe
 import io.kotlintest.specs.WordSpec
-import utils.SOLUTION_HERE
 
 // tag::init[]
 fun increment(xs: List<Int>): List<Int> =
+    foldRight(xs, List.empty()) { n, acc -> Cons(n + 1, acc) }
 
-    SOLUTION_HERE()
 // end::init[]
 
 //TODO: Enable tests by removing `!` prefix
 class Exercise15 : WordSpec({
     "list increment" should {
-        "!add 1 to every element" {
+        "add 1 to every element" {
             increment(List.of(1, 2, 3, 4, 5)) shouldBe
                 List.of(2, 3, 4, 5, 6)
         }

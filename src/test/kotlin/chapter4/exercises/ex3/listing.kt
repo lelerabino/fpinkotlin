250cdad6782cd2019ca33bb208de2c196b9728f0
lelerabino <lelerabino@gmail.com>
2022-01-07 16:32:02 +0100

    feat: Option map2 exercise

--- src/test/kotlin/chapter4/exercises/ex3/listing.kt
+++ src/test/kotlin/chapter4/exercises/ex3/listing.kt
@@ -5,12 +5,20 @@ import chapter4.Option
 import chapter4.Some
 import io.kotlintest.shouldBe
 import io.kotlintest.specs.WordSpec
-import utils.SOLUTION_HERE
 
 //tag::init[]
-fun <A, B, C> map2(a: Option<A>, b: Option<B>, f: (A, B) -> C): Option<C> =
+fun <A, B, C> map2(
+    oa: Option<A>,
+    ob: Option<B>,
+    f: (A, B) -> C
+): Option<C> =
+    oa.flatMap { a -> ob.map { b -> f(a, b) } }
 
-    SOLUTION_HERE()
+fun <A, B, C> map2_by_matching(a: Option<A>, b: Option<B>, f: (A, B) -> C): Option<C> =
+    when {
+        a is Some && b is Some -> Some(f(a.get, b.get))
+        else -> None
+    }
 //end::init[]
 
 //TODO: Enable tests by removing `!` prefix
@@ -22,13 +30,13 @@ class Exercise3 : WordSpec({
         val b = Some(20)
         val none = Option.empty<Int>()
 
-        "!combine two option values using a binary function" {
+        "combine two option values using a binary function" {
             map2(a, b) { aa, bb ->
                 aa * bb
             } shouldBe Some(100)
         }
 
-        "!return none if either option is not defined" {
+        "return none if either option is not defined" {
             map2(a, none) { aa, bb ->
                 aa * bb
             } shouldBe None
